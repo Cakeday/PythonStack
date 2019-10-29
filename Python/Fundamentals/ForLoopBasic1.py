4ca5a7b2b45fddca1cb3520e9cca2deb61a70b51
Caleb Reyes <sciencecamp123@gmail.com>
2019-10-28 18:12:20 -0700

    Adding ForLoopBasic assignment

---
+++ Python/Fundamentals/ForLoopBasic1.py
@@ -0,0 +1,42 @@
+# def basicPrint():
+#     for val in range(151):
+#         print(val)
+# basicPrint()
+
+# def print5():
+#     for val in range(5,1001,5):
+#         print(val)
+# print5()
+
+# def counting():
+#     val=1
+#     while val<101:
+#         if val%10 == 0:
+#             print("Coding Dojo")
+#             val+=1
+#             continue
+#         elif val%5==0:
+#             print("Coding")
+#             val+=1
+#             continue
+#         print(val)
+#         val+=1
+# counting()
+
+# def sucker():
+#     sum = 0
+#     for val in range(1,500000,2):
+#         sum+=val
+#     print(sum)
+# sucker()
+
+# def fourDown():
+#     for val in range(2018,0,-4):
+#         print(val)
+# fourDown()
+
+# def flex(lowNum, highNum, mult):
+#     for val in range (lowNum, highNum):
+#         if val%mult == 0:
+#             print(val)
+# flex(2,3457,18)
