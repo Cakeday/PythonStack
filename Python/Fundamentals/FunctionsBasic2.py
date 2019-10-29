85236cd9386a8623026c3a9ab640f2af591299b4
Caleb Reyes <sciencecamp123@gmail.com>
2019-10-28 19:33:41 -0700

    Added FunctionsBasic2

---
+++ Python/Fundamentals/FunctionsBasic2.py
@@ -0,0 +1,30 @@
+# def countdown(num):
+#     list = []
+#     for val in range (num,-1, -1):
+#         list.append(val)
+#     return list
+# print(countdown(8))
+
+# def sauce(list):
+#     print(list[0])
+#     return list[1]
+# print(sauce([0,1]))
+
+# def fpl(list):
+#     return list[0]+len(list)
+# print(fpl([3,7]))
+
+# def vgs(list):
+#     if len(list) < 2:
+#         return False
+#     list.pop(0)
+#     list.pop(0)
+#     return list
+# print(vgs([3,7,67,34,6,7]))
+
+# def tltv(size, value):
+#     list = []
+#     for val in range (size):
+#         list.append(value)
+#     return list
+# print(tltv(6,3))
