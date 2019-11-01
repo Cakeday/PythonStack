3eb70c6b051d1942493b6aa3a3cdcb393e252efd
Caleb Reyes <sciencecamp123@gmail.com>
2019-10-31 17:58:51 -0700

    Updated HelloFlask

--- Flask/FlaskFundamentals/HelloFlask/Hello.py
+++ Flask/FlaskFundamentals/HelloFlask/Hello.py
@@ -28,6 +28,18 @@ def so_many_words(num, word):
         repeat += f'<h3>{word}</h3>'
     return repeat
 
+@app.route('/lists')
+def render_lists():
+    student_info = [
+       {'name' : 'Michael', 'age' : 35},
+       {'name' : 'John', 'age' : 30 },
+       {'name' : 'Mark', 'age' : 25},
+       {'name' : 'KB', 'age' : 27}
+    ]
+    return render_template("index.html", random_numbers = [3,1,5], students = student_info)
+
+
+
 
 
 if __name__=="__main__": 
