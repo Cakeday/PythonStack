3b396a1bb599442157bbcf146d41009db05fb5be
Caleb Reyes <sciencecamp123@gmail.com>
2019-11-01 19:45:45 -0700

    Updated my FormTest folder

--- Flask/FlaskFundamentals/FormTest/server.py
+++ Flask/FlaskFundamentals/FormTest/server.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
+app.secret_key = 'whoa security'
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
@@ -14,11 +15,15 @@ def index():
 def create_user():
     print("Got Post Info")
     print(request.form)
-    name_from_form = request.form['name']
-    email_from_form = request.form['email']
-    return render_template("show.html", name_on_template=name_from_form, email_on_template=email_from_form)
+    session['username']=request.form['name'] 
+    session['useremail']=request.form['email']
+    return redirect("/show")
 
-
+@app.route('/show')
+def show_user():
+    print("Showing the user info from the form")
+    print(request.form)
+    return render_template("show.html", name_on_template=session['username'], email_on_template=session['useremail'])
 
 
 
