c23a8ac1882c6f547c75a11b6fc5e4b0fee49a85
Caleb Reyes <sciencecamp123@gmail.com>
2019-11-05 12:09:44 -0800

    Added the TimeDisplay assignment

--- Django/DjangoIntro/TimeDisplay/apps/CurrentTime/views.py
+++ Django/DjangoIntro/TimeDisplay/apps/CurrentTime/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, HttpResponse
+from time import gmtime, strftime
 
 def index(request):
-    return render(request, 'CurrentTime/index.html')
+    context = {
+        "calendar" : strftime("%b %d, %Y", gmtime()),
+        "hour" : strftime("%I:%M %p", gmtime())
+    }
+    return render(request, 'CurrentTime/index.html', context)
 
 # Create your views here.
