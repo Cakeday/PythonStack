fbcec24077c5a975724d0ada0934861269d7b169
Caleb Reyes <sciencecamp123@gmail.com>
2019-11-08 18:47:24 -0800

    Added the DojosNinjas Django ORM assignment

--- Django/DjangoORM/DojoNinjas/apps/DojoNinjasApp/models.py
+++ Django/DjangoORM/DojoNinjas/apps/DojoNinjasApp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Dojo(models.Model):
+    name = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+    desc = models.TextField()
+
+    def __repr__(self):
+        return f"<Dojo object: {self.name} {self.city},{self.state}>"
+
+class Ninja(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    dojo = models.ForeignKey(Dojo, related_name='ninjas')
+
+    def __repr__(self):
+        return f"<Ninja object: {self.first_name} {self.last_name} {self.dojo}>"
+
