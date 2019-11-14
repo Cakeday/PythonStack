61971c4729c78a1c0c344fd991e2980b58b69667
Caleb Reyes <sciencecamp123@gmail.com>
2019-11-13 19:57:54 -0800

    Added Validations

--- Django/DjangoORM/SemiRestfulTV/apps/TVShows/models.py
+++ Django/DjangoORM/SemiRestfulTV/apps/TVShows/models.py
@@ -1,4 +1,19 @@
 from django.db import models
+from datetime import datetime
+
+
+class ShowManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        if len(postData['title'])<2:
+            errors['title'] = "Yo put a longer title ya dingus"
+        if len(postData['network'])<3:
+            errors['network'] = "Yo put a longer network ya dingbat"
+        if postData['release_date']>datetime.now().strftime("%Y-%m-%d"):
+            errors['release_date'] = "what is this, back to the future?"
+        if len(postData['description'])<10:
+            errors['description'] = "dude just put a longer description (or no description)"
+        return errors
 
 class Show(models.Model):
     title = models.CharField(max_length=255)
@@ -7,4 +22,4 @@ class Show(models.Model):
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-# Create your models here.
+    objects = ShowManager()
--- Django/DjangoORM/SemiRestfulTV/apps/TVShows/views.py
+++ Django/DjangoORM/SemiRestfulTV/apps/TVShows/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render, redirect
 from .models import Show
+from django.contrib import messages
 
 def index(request):
     if request.method == 'GET':
-        return redirect('/shows')
+        return render(request, 'TVShows/index.html')
 
 def create(request):
     if request.method == 'POST':
+        errors = Show.objects.basic_validator(request.POST)
+        if len(errors) > 0:
+            for key, value in errors.items():
+                messages.error(request, value)
+            return redirect('/shows/new')
         Show.objects.create(title=request.POST['title'], network=request.POST['network'], release_date=request.POST['release_date'], description=request.POST['description'])
         new=Show.objects.last()
         return redirect(f'/shows/{new.id}')
@@ -34,13 +40,19 @@ def edit(request, id):
 
 def update(request):
     if request.method == 'POST':
+        errors = Show.objects.basic_validator(request.POST)
+        if len(errors) > 0:
+            for key, value in errors.items():
+                messages.error(request, errors[key])
+            return redirect('/shows/'+request.POST['id']+'/edit')
         show_to_update = Show.objects.get(id=request.POST['id'])
         show_to_update.title = request.POST['title']
         show_to_update.network = request.POST['network']
         show_to_update.release_date = request.POST['release_date']
         show_to_update.description = request.POST['description']
         show_to_update.save()
-        return redirect(f'/shows/{show_to_update.id}')
+        # return redirect(f'/shows/{show_to_update.id}')
+        return redirect(f"/shows/{show_to_update.id}")
 
 def destroy(request, id):
     if request.method == 'GET':
