29384640e98542e4ae99f4cdae5af972557a0880
Caleb Reyes <sciencecamp123@gmail.com>
2019-12-09 19:09:12 -0800

    Changed the __futures__ in the models.py

--- Django/DjangoORM/BooksAuthors/apps/BooksAuthorsApp/models.py
+++ Django/DjangoORM/BooksAuthors/apps/BooksAuthorsApp/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from __future__ import unicode_literals
+# from __future__ import unicode_literals
 
 class Book(models.Model):
     title = models.CharField(max_length=255)
